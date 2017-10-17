ea93624c8c870b75e56316b9035be8bf5ee7b7ad
maxdesp <maxdesp@free.fr>
2017-10-17 18:10:55 +0200

    add accessors, change statement and connection to attributes

--- src/dao/DB.java
+++ src/dao/DB.java
@@ -22,85 +22,111 @@ public class DB {
 	private String dbUser;
 	
 	public DB() throws SQLException, ClassNotFoundException{
-		this.dbName = "chat";
-		this.dbUser = "root";
+		Class.forName("com.mysql.jdbc.Driver");
+		this.setDbName("chat");
+		this.setDbUser("root");
 		// this.dbUser ="chatuser";
-		this.connection = getConnection("chat", "root");
-		this.statement = getStatement();
+		this.connection = makeConnection(this.getDbName(), this.getDbUser());
+		this.statement = this.connection.createStatement();
 	}
-	public Connection getConnection() {
-		return connection;
-	}
-
-	public void setConnection(Connection connection) {
-		this.connection = connection;
-	}
-
-	public void setStatement(Statement statement) {
-		this.statement = statement;
-	}
-
-
-
 	
+
+
+	// useless if main exists ?
 	public static void main() {
-		
 	}
 	
-	public static Connection getConnection(String base, String user) throws SQLException, ClassNotFoundException {
+	public Connection makeConnection(String base, String user) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+ base +"?useSSL=false", user, "");
 		return con;
 		
 	}
-	public static Statement getStatement() {
+	
+	
+	/** useless ?
+
+	
+	public Statement makeStatement() {
 		try {
+			// Class.forName("com.mysql.jdbc.Driver");
 			
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			Connection  eshopConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/eshop?useSSL=false", "root", "");
-			Statement eshopStatement = eshopConnection.createStatement();
+			//Connection  eshopConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/eshop?useSSL=false", "root", "");
+			// Statement eshopStatement = eshopConnection.createStatement()
 			//ResultSet myResult = eshopStatement.executeQuery("SELECT PER_ID, PER_CP FROM personne");
-			return eshopStatement;
+			return this.connection.createStatement();
 		} catch (Exception ex) {}
 		return null;
 		
 	}
+	 */
 	
-	public static ResultSet executeQuery(Statement statement, String query) throws SQLException {
+	public ResultSet executeQuery(String query) throws SQLException {
 		Io.print(query);
-		return statement.executeQuery(query);
+		return this.statement.executeQuery(query);
 		
 	}
-	public static boolean execute(Statement statement, String query) throws SQLException {
+	public boolean execute(String query) throws SQLException {
 		Io.print(query);
-		return statement.execute(query);
+		return this.statement.execute(query);
 		
 	}
 	
-	public static ResultSet getByAttribute(Statement statement, String table, String attribute, Integer id) throws SQLException {
+	/**
+	 * 
+	 * @param table			table to select from
+	 * @param attribute		attribute, i.e. "UTI_ID"
+	 * @param id			must be a valid id in 'database.table'
+	 */
+	public ResultSet getById(String table, String attribute, Integer id) throws SQLException {
 		String query = "SELECT * FROM " + table +  " WHERE " + attribute + " = " + id;
-		ResultSet results =  executeQuery(statement, query);
+		ResultSet results =  this.executeQuery(query);
 		if (results.next()){
 			return results;
 		}
 		return null;
 	}
 	
-	public static ResultSet getFromTable(Statement statement, String column, String table) throws SQLException {
+	/**
+	 *  get single column as String from table
+	 */
+	public ResultSet getFromTable(String column, String table) throws SQLException {
 		String query = "SELECT " + column + " FROM " + table;
-		return executeQuery(statement, query);
+		return this.executeQuery(query);
  	}
 	
-	public static ResultSet getFromTable(Statement statement, String[] columns, String table) throws SQLException {
+	/**
+	 *  get multiple columns as String[] from table 
+	 */
+	public ResultSet getFromTable(String[] columns, String table) throws SQLException {
 		String joined = String.join(", ", columns);
 		String query = "SELECT " + joined + " FROM " + table;
-		return executeQuery(statement, query);
+		return this.executeQuery(query);
  	}
 
 	
-	public static void setAttribute(Statement statement, String attribute, String table) {
-		
+	// accessors
+	public String getDbUser() {
+		return dbUser;
 	}
+	public void setDbUser(String dbUser) {
+		this.dbUser = dbUser;
+	}
+	public String getDbName() {
+		return dbName;
+	}
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+	public Connection getConnection() {
+		return connection;
+	}
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	public void setStatement(Statement statement) {
+		this.statement = statement;
+	}
+
 
 }
