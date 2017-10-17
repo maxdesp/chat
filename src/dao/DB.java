265f00edf211426ae86702ac202bfc3f0f54b67c
maxdesp <maxdesp@free.fr>
2017-10-17 16:32:07 +0200

    DB DAO

---
+++ src/dao/DB.java
@@ -0,0 +1,100 @@
+package dao;
+
+// import IO;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import com.mysql.jdbc.Driver;
+
+import main.Io;
+
+public class DB {
+	
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public void setStatement(Statement statement) {
+		this.statement = statement;
+	}
+
+
+	private Connection connection;
+	private Statement statement;
+	
+	public DB() throws SQLException, ClassNotFoundException{
+		this.connection = getConnection("chat", "root");
+		this.statement = getStatement();
+	}
+	
+	public static void main() {
+		
+	}
+	
+	public static Connection getConnection(String base, String user) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+ base +"?useSSL=false", user, "");
+		return con;
+		
+	}
+	public static Statement getStatement() {
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection  eshopConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/eshop?useSSL=false", "root", "");
+			Statement eshopStatement = eshopConnection.createStatement();
+			//ResultSet myResult = eshopStatement.executeQuery("SELECT PER_ID, PER_CP FROM personne");
+			return eshopStatement;
+		} catch (Exception ex) {}
+		return null;
+		
+	}
+	
+	public static ResultSet executeQuery(Statement statement, String query) throws SQLException {
+		Io.print(query);
+		return statement.executeQuery(query);
+		
+	}
+	public static boolean execute(Statement statement, String query) throws SQLException {
+		Io.print(query);
+		return statement.execute(query);
+		
+	}
+	
+	public static ResultSet getByAttribute(Statement statement, String table, String attribute, Integer id) throws SQLException {
+		String query = "SELECT * FROM " + table +  " WHERE " + attribute + " = " + id;
+		ResultSet results =  executeQuery(statement, query);
+		if (results.next()){
+			return results;
+		}
+		return null;
+	}
+	
+	public static ResultSet getFromTable(Statement statement, String column, String table) throws SQLException {
+		String query = "SELECT " + column + " FROM " + table;
+		return executeQuery(statement, query);
+ 	}
+	
+	public static ResultSet getFromTable(Statement statement, String[] columns, String table) throws SQLException {
+		String joined = String.join(", ", columns);
+		String query = "SELECT " + joined + " FROM " + table;
+		return executeQuery(statement, query);
+ 	}
+
+	
+	public static void setAttribute(Statement statement, String attribute, String table) {
+		
+	}
+
+}
---
+++ src/main/Io.java
@@ -0,0 +1,60 @@
+package main;
+public class Io {
+//	
+//	public static void printAll(String...strings) {
+//		for (int i = 0; i < algo.len(strings); i++) {
+//			System.out.print(strings[i]);
+//			System.out.print(" - ");
+//		}
+//	}
+
+	static void print(char[] a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+
+
+	public static void print(Object a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+
+	static void print(int a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+	
+	static void print(boolean a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+	
+//	static void print(Color a)
+//		System.out.print(a);
+//		System.out.print("\n");
+//	}
+	static void print(String a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+//	static void print(Str a) {
+//		System.out.print(a);
+//		System.out.print("\n");
+//	}
+	
+	static void print(String[] a) {
+		System.out.print(a);
+		System.out.print("\n");
+	}
+//	static void print(int[] monTab){
+//	       for (int i=0; i< algo.len(monTab); i++){
+//	    	   System.out.print(monTab[i]);
+//	    	   System.out.print(" ");
+//	       }
+//	       Io.print("\n");
+//	   }
+	
+	public static void niet() {
+		Io.print ("NIET : Not implemented Error Triggered");
+	}
+}
