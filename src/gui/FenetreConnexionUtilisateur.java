5167e7730a381b10e76666c9c6f95a7aebdfcf13
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-10-18 08:51:03 +0200

    Correction nom package

--- src/gui/FenetreConnexionUtilisateur.java
+++ src/gui/FenetreConnexionUtilisateur.java
@@ -1,4 +1,4 @@
-package View;
+package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/gui/FenetrePrincipale.java
+++ src/gui/FenetrePrincipale.java
@@ -1,4 +1,4 @@
-package View;
+package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
