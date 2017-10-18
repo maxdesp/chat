d0c4d80fbae69129bad700d03e30fd68bfae59f6
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-10-18 08:59:23 +0200

    Update gui

--- src/gui/FenetreConnexionUtilisateur.java
+++ src/gui/FenetreConnexionUtilisateur.java
@@ -3,12 +3,23 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class FenetreConnexionUtilisateur extends JFrame implements ActionListener {
 
-	//private JLabel 
+	private JLabel labelConnexion = new JLabel("Connexion au profil");
+	private JLabel labelPseudo = new JLabel("Pseudo");
+	private JTextField textFieldPseudo = new JTextField("Ecrivez votre pseudo");
+	private JLabel labelMDP = new JLabel("Mot de passe");
+	private JTextField textFieldMDP = new JTextField("Ecrivez votre mot de passe");
+	private JLabel labelAvatar = new JLabel("Avatar");
+	private JTextField textFieldAvatar = new JTextField("URL de votre avatar");
+	private JButton boutonConnexion = new JButton("Connexion");
+	private JButton boutonAnnuler = new JButton("Annuler");
 	
 	public FenetreConnexionUtilisateur(){
 		this.setTitle("Fenetre de connexion");
