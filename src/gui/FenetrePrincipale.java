ed32f76464572786993a9d4844c93616d1375b5a
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-10-19 08:49:41 +0200

    l

--- src/gui/FenetrePrincipale.java
+++ src/gui/FenetrePrincipale.java
@@ -148,7 +148,7 @@ public class FenetrePrincipale extends JFrame implements ActionListener, KeyList
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.contentPane.setLayout(layoutPrincipal);
 		this.setJMenuBar(menuBar);
-		this.menuBar.setBackground(new Color(255*new Random().nextInt(2),255*new Random().nextInt(2),255*new Random().nextInt(2)));
+		this.menuBar.setBackground(new Color(25*new Random().nextInt(11),25*new Random().nextInt(11),25*new Random().nextInt(11)));
 		this.menuBar.add(menuFichier);
 		this.menuBar.add(menuUtilisateur);
 		this.menuUtilisateur.add(menuConnexionUtilisateur);
